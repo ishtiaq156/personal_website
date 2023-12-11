b8b1982001fd8a61cd02531ad25fbdc269e55e2f
Ishtiaq Ul Haq Syed <ishtiaq156@gmail.com>
2023-12-10 22:19:27 -0500

    Add '@vercel/analytics' dependency and update layout.js
    This commit integrates the '@vercel/analytics' package into the application, as seen in the modifications to 'package.json'. The 'layout.js' file has been updated to incorporate the 'Analytics' component from '@vercel/analytics/react', enhancing the ability to track and analyze user interactions on the webpages.

--- components/layout.js
+++ components/layout.js
@@ -14,6 +14,7 @@ import { useEffect } from 'react'
 import { useLocalStorage } from '../hooks/useLocalStorage'
 import dynamic from 'next/dynamic'
 import { SpeedInsights } from '@vercel/speed-insights/next'
+import { Analytics } from '@vercel/analytics/react'
 
 const DarkModeToggle = dynamic(
   function DarkModeToggle() {
@@ -159,6 +160,7 @@ export default function Layout({ children }) {
         </motion.div>
       </div>
       <SpeedInsights />
+      <Analytics />
     </div>
   )
 }
